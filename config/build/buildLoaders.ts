39ad39b8a0cd76bd980a96d6a4e3c49915b9d537
Yuriy <yrii.petrik@gmail.com>
2023-05-10 12:05:02 +0700

    Настроил css modules in webpack loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,17 +1,31 @@
 import webpack from 'webpack'
-export function builderLoaders(): webpack.RuleSetRule[] {
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import { BuildOptions } from './types/config'
+
+export function builderLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
 
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
 			// Creates `style` nodes from JS strings
-			"style-loader",
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
 			// Translates CSS into CommonJS
-			"css-loader",
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev
+							? '[path][name]__[local]--[hash:base64:5]'
+							: '[hash:base64:8]'
+					},
+				}
+			},
 			// Compiles Sass to CSS
 			"sass-loader",
 		],
+
 	}
 
 	// Если не использую ts -- нужно подключить bable-loader
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import './Counter.scss';
-
+import classes from './Counter.module.scss';
+ 
 const Counter = () => {
   const [count, setCount] = React.useState(0);
 
   const increment = () => [setCount((prev) => prev + 1)];
   return (
-    <div>
+    <div className={classes.button}>
       <h1> {count}</h1>
       <button onClick={increment}>increment</button>
     </div>
