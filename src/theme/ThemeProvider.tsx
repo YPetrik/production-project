10967ac6ba7ee6c944df0f1a5dd5001d2916a1f6
Yuriy <yrii.petrik@gmail.com>
2023-05-27 11:53:20 +0700

    1.8_added classNames

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+const ThemeProvider: React.FC = ({children}) => {
+  const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.DARK;
+  const [theme, setTheme] = React.useState<Theme>(defaultTheme);
+
+  const defaultProps = React.useMemo(
+    () => ({
+      theme: theme,
+      setTheme: setTheme,
+    }),
+    [theme],
+  );
+  return <ThemeContext.Provider value={defaultProps}>
+		{children }
+	</ThemeContext.Provider>;
+};
+
+export default ThemeProvider;
